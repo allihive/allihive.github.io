34a0e2fd34832295f4099eda9be7967ff19c25f5
Alice Li <aliceli.bls@gmail.com>
2025-09-03 12:56:37 +0300

    Header added to portfolio

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            <h1>Alice Li Maunumäki</h1>
+            <nav>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
