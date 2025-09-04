f8b18e867c958e22ec47a355e97dc714a6b4aec8
Alice Li <aliceli.bls@gmail.com>
2025-09-04 14:42:38 +0300

    working on about section

---
+++ src/components/About.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import "./About.css"
+
+const About: React.FC = () => {
+    return (
+        <section id="about" className="about">
+            <div className="about-content">
+                <h2>About Me</h2>
+                <p>
+                    Hello, my name is Alice!
+                </p>
+                <p>
+                    I love software development especially when it comes to designing and coding websites! I recently finished the curriculum from Hive Helsinki (a 42 school) where we studied low level languages, C and C++, to understand how the software and hardware communicate.
+                </p>
+                <p>
+                    When I'm not coding, I enjoy knitting, crocheting, fashion styling, swimming in the Baltic Sea, and biking!
+                </p>
+                {/* <div className="about-image">
+                    <img src={} alt="Alice's face"></img>
+                </div> */}
+            </div>
+        </section>
+    )
+}
+
+export default About
