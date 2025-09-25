3c0de4d46756069a352cde575f26a10e9260d2ca
Alice Li Maunumäki <aliceli@Alices-MacBook-Pro.local>
2025-09-25 14:30:32 +0300

    changed theme of the website

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,21 @@
-import React from "react"
-import port from "../assets/logo/Portfolio.svg"
-import folio from "../assets/logo/folio.svg"
-import portrait from "../assets/IMG_3152.jpg"
-import "./Hero.css"
+import React from "react";
+import alice from "../assets/logo/alice.svg";
+import port from "../assets/logo/port.svg";
+import folio from "../assets/logo/folio.svg";
+// import portrait from "../assets/IMG_3152.jpg"
+// import darkPortrait from "../assets/IMG_3153.jpg";
+import "./Hero.css";
 
 const Hero: React.FC = () => {
-	return (
-		<section id="hero" className="hero">
-			<img src={port}></img>
-			<div className="hero-image">
-				<img src={portrait}></img>
-			</div>
-			<img src={folio}></img>
-		</section>
-	)
-}
+  return (
+    <section id="hero" className="hero">
+          <img src={port} className="port"></img>
+          <div className="hero-image">
+			<img src={alice} className="alice"></img>
+          </div>
+          <img src={folio} className="folio"></img>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
